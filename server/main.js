d0a95fb8b20585d5970dc20315300aa6be26a93b
seth-way <seth.r.way@gmail.com>
2022-11-06 23:06:30 -0500

    fix port error again

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const server = require('./index');
 const { db } = require('./db');
+const { eventNames } = require('./index');
 
 db.sync().then(() => {
   server.listen(PORT, () =>
